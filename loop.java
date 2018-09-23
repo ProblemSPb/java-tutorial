34e32cf5d19a2bbf509da67fc45e7e66c9686510
ProblemSPb <ussr83@mail.ru>
2018-09-23 12:45:25 +0200

    new data

--- loop.java
+++
@@ -1,9 +0,0 @@
-//
-// foreach loop
-//
-
-int[]arr = {2, 0, 1, 3};
-
-for(int el:arr){
-    System.out.println(el);
-}
---
+++ src/DArray.java
@@ -0,0 +1,19 @@
+public class DArray {
+
+    public static void main(String[] args) {
+        int [][] nameArray = new int [5][10];
+
+        for (int x = 0; x < nameArray.length; x++) {
+            for (int y = 0; y < 10; y++) {
+                nameArray[x][y]=y;
+            }
+        }
+
+        for (int x = 0; x < nameArray.length; x++) {
+            for (int y = 0; y < 10; y++) {
+                System.out.print(nameArray[x][y] +" ");
+            }
+            System.out.println();
+        }
+    }
+}
