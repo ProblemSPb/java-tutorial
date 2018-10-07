dcc14a10c1ff030b1f653336d2bd9913ae2fc656
ProblemSPb <ussr83@mail.ru>
2018-10-07 21:56:15 +0200

    Triangle / 2D array

---
+++ src/DArrayTraingle.java
@@ -0,0 +1,19 @@
+public class DArrayTraingle {
+
+    public static void main(String[] args) {
+
+        int[][] triangle = new int[5][9];
+
+        for ()
+
+        // printing of the array below
+
+        for (int i = 0; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[0].length; j++) {
+                System.out.print(triangle[i][j] + " ");
+            }
+            System.out.println();
+        } // end of printing the array
+
+    }
+}
