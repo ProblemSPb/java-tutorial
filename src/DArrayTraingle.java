3ab18bf51ddc2e574fc5ec4007884cf1820a7e50
ProblemSPb <ussr83@mail.ru>
2018-10-18 16:32:38 +0200

    array 1/0 triangle

--- src/DArrayTraingle.java
+++ src/DArrayTraingle.java
@@ -5,16 +5,30 @@ public class DArrayTraingle {
         int[][] triangle = new int[5][9];
 
 
-        // printing of the array below
+        for (int x = 1; x < 5; x++) {
+            for (int y = x; y < (triangle[x].length - x); y++ ) {
+                triangle[x][y] = 1;
+            }
+        }
+
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 9; y++) {
+                System.out.print(triangle[x][y] + " ");
+            }
+            System.out.println();
+        }
+
+
+        /* printing of the array below
 
         for (int i = 0; i < triangle.length; i++) {
             for (int j = 0; j < triangle[0].length; j++) {
                 System.out.print(triangle[i][j] + " ");
             }
             System.out.println();
-        } // end of printing the array
+        }  end of printing the array */
 
     }
 }
 
-// not finished
+// array 1/0 triangle
