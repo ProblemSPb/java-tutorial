2710796510a43081279987a067d35a28370da4d8
ProblemSPb <ussr83@mail.ru>
2018-11-04 19:01:24 +0100

    Babble sort in an array

--- src/bubbleSortHeight.java
+++ src/bubbleSortHeight.java
@@ -2,14 +2,14 @@ public class bubbleSortHeight {
 
     public static void main(String[] args) {
 
-        int [] height = {150, 180, 189, 130, 168, 155};
+        int [] height = {150, 180, 67, 189, 130, 168, 155, 170};
 
 
-        for (int i = 0; i < height.length-1; i++)
+        for (int i = 0; i < height.length; i++)
             for (int j = 0; j < height.length-i-1; j++)
                 if (height[j] > height[j+1])
                 {
-                    // swap temp and arr[i]
+                    // swap temp index and array[i]
                     int temp = height[j];
                     height[j] = height[j+1];
                     height[j+1] = temp;
