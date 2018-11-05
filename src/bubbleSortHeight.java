f5a66129334133abc3537a661de97690908952d8
ProblemSPb <ussr83@mail.ru>
2018-11-05 19:36:54 +0100

    Bubble sort in an array [for and while]

--- src/bubbleSortHeight.java
+++ src/bubbleSortHeight.java
@@ -5,16 +5,31 @@ public class bubbleSortHeight {
         int [] height = {150, 180, 67, 189, 130, 168, 155, 170};
 
 
-        for (int i = 0; i < height.length; i++)
-            for (int j = 0; j < height.length-i-1; j++)
-                if (height[j] > height[j+1])
-                {
-                    // swap temp index and array[i]
-                    int temp = height[j];
-                    height[j] = height[j+1];
-                    height[j+1] = temp;
+        for(int i = 0; i < height.length; i++) {
+            for(int j = 0; j < height.length; j++) {
+                if(height[i] < height[j]) {
+                    int temp = height[i];
+                    height[i] = height[j];
+                    height[j] = temp;
                 }
+            }
+        }
 
+/*
+        boolean swapped = true;
+        while(swapped) {
+            swapped = false;
+            for(int i = 0; i < height.length - 1; i++) {
+                if(height[i] > height[i + 1]) {
+                    int temp = height[i];
+                    height[i] = height[i + 1];
+                    height[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        }
+
+*/
         for (int i = 0; i < height.length; i++) {
 
             System.out.print(height[i] + " ");
